2ddf70b2ec633757727443ff932c18cf4dbfc625
helton <heltonapp@gmail.com>
2024-08-01 14:45:09 -0400

    create auth domain

---
+++ src/main/java/devhelton/services/AuthorizationService.java
@@ -0,0 +1,18 @@
+package devhelton.services;
+
+import devhelton.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+public class AuthorizationService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+}
